857b9d081332d57e87d56abe8cbd3b08649af925
Harshad Mane <harshad.mane@pubmatic.com>
2019-03-12 16:52:50 -0700

    Update main.go

--- main.go
+++ main.go
@@ -3,7 +3,5 @@ package main
 func main() {
 	domainToDbmProviderMap := generateDbmMap(readFile("DataFiles/DBM_Providers.csv") )
 	domainToIabVendorMap := generateIabMap(readFile("DataFiles/vendor-list.json"))
-	domainToDbm250ProviderMap := generateDbmMap(readFile("DataFiles/DBM_PM_250_Providers.csv") )
-	//simpleMapCsvOutput(domainToDbmProviderMap, domainToIabVendorMap)
-	staticListMapCsvOutput(domainToDbmProviderMap, domainToIabVendorMap, domainToDbm250ProviderMap)	
+	simpleMapCsvOutput(domainToDbmProviderMap, domainToIabVendorMap)
 }
